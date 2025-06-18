e47389c6cf4f020a3c5b82d557040db15148d446
minseokkim-web <nuspha123@naver.com>
2025-06-18 17:17:57 +0900

    0618 up

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-
+AOS.init();
 document.addEventListener("DOMContentLoaded",function(){
     
     const progressBar =  document.querySelectorAll(".progress-bar");
@@ -10,15 +10,40 @@ document.addEventListener("DOMContentLoaded",function(){
     })
 
 
+    const searchInput = document.getElementById("searchInput");
+    const portfolioitems = document.querySelectorAll(".portfolio-item");
 
+    searchInput.addEventListener("input", function(){
+        const searchTerm = this.value.toLowerCase();
+        portfolioitems.forEach((item) => {
+            const title = item.querySelector("h4").textContent.toLowerCase();
+            if(title.includes(searchTerm)) {
+                item.style.display = "block";
+            } else {
+                item.style.display = "none";
+            }
+        })
 
+    })
 
+    const filterButtons = document.querySelectorAll(".filter-btn")
 
+    filterButtons.forEach((button) => {
+        button.addEventListener("click", function(){
+            const filter = this.getAttribute("data-filter");
+            portfolioitems.forEach((item) => {
+                if(filter === 'all' || item.getAttribute("data-category")
+                === filter) {
+              item.style.display = "block";
+            } else {
+                item.style.display = "none";
+            }
+        })
+       })
+    })   
 
-
-
-
-
+    
+        
 
 
 
